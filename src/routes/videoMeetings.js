eef0407eae25df38aee9c1b9044d873f033d4d7d
Getgingee <pankaj@getgingee.com>
2025-12-06 15:08:21 +0530

    fix: destructure authMiddleware import in videoMeetings routes

--- src/routes/videoMeetings.js
+++ src/routes/videoMeetings.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const router = express.Router();
 const videoMeetingController = require('../controllers/videoMeetingController');
-const authMiddleware = require('../middleware/auth');
+const { authMiddleware } = require('../middleware/auth');
 const { requireTier } = require('../middleware/tierAccess');
 
 // All routes require authentication
