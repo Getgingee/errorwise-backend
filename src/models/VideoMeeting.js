acbe74cec664248a7bfc489b9eb8c244ad0a5cd3
Getgingee <pankaj@getgingee.com>
2025-12-06 14:56:16 +0530

    fix: VideoMeeting.js wrong database import causing deployment crash

--- src/models/VideoMeeting.js
+++ src/models/VideoMeeting.js
@@ -4,7 +4,7 @@
  */
 
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/database');
 
 const VideoMeeting = sequelize.define('VideoMeeting', {
   id: {
