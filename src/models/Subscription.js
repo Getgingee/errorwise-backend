22e37f8d6cf6973ab7b82921be09b9e6c2e2ebd1
Getgingee <pankaj@getgingee.com>
2025-12-07 17:24:15 +0530

    fix: update Subscription model to use UUID types matching users table

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -2,29 +2,34 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Subscription = sequelize.define('Subscription', {
-  subscriptionId: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    field: 'subscription_id'
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
   },
   userId: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.UUID,
     allowNull: false,
-    field: 'user_id'
+    field: 'user_id',
+    references: {
+      model: 'users',
+      key: 'id'
+    },
+    onDelete: 'CASCADE'
   },
   planId: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING(50),
     allowNull: true,
+    defaultValue: 'free',
     field: 'plan_id'
   },
   tier: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(50),
     allowNull: true,
-    field: 'subscription_tier'
+    defaultValue: 'free'
   },
   status: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(50),
     defaultValue: 'active'
   },
   startDate: {
@@ -38,17 +43,17 @@ const Subscription = sequelize.define('Subscription', {
     field: 'end_date'
   },
   trialStart: {
-    type: DataTypes.DATEONLY,
+    type: DataTypes.DATE,
     allowNull: true,
     field: 'trial_start'
   },
   trialEnd: {
-    type: DataTypes.DATEONLY,
+    type: DataTypes.DATE,
     allowNull: true,
     field: 'trial_end'
   },
   nextBillingDate: {
-    type: DataTypes.DATEONLY,
+    type: DataTypes.DATE,
     allowNull: true,
     field: 'next_billing_date'
   },
@@ -63,17 +68,17 @@ const Subscription = sequelize.define('Subscription', {
     field: 'cancelled_at'
   },
   dodoSubscriptionId: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(255),
     allowNull: true,
     field: 'dodo_subscription_id'
   },
   dodoCustomerId: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(255),
     allowNull: true,
     field: 'dodo_customer_id'
   },
   paymentMethod: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(255),
     allowNull: true,
     field: 'payment_method'
   },
@@ -86,8 +91,7 @@ const Subscription = sequelize.define('Subscription', {
   tableName: 'subscriptions',
   timestamps: true,
   underscored: true
-  // Note: Indexes already exist in database, don't define here to avoid duplicate creation errors
-  // Existing indexes: subscriptions_user_id, subscriptions_status, subscriptions_dodo_subscription_id
+  // Note: Indexes created in database migration, not defined here to avoid duplicate creation errors
 });
 
 module.exports = Subscription;
